455278880566e7014b5188faa1f2a60f4f2576f7
Thomas Hodgkinson <thomashodgkinson1987@gmail.com>
2025-08-22 23:14:02 +0100

    feat(actor): Overhaul Actor API for const correctness and mutability
    Reworks the entire Actor API to be more robust, encapsulated, and easier to use.
    
    - Changes `add_component` functions to take raw data instead of pre-created components, clarifying ownership.
    - Implements a `const`-correct API with `_mut` variants for safe, explicit mutation of components.
    - Adds a full suite of getters and setters for actor properties like position, glyph, and colour.
    - Adds error checking to component management functions to prevent adding duplicate components.

--- include/actor.h
+++ include/actor.h
@@ -1,7 +1,6 @@
 #ifndef ACTOR_H
 #define ACTOR_H
 
-#include <stdbool.h>
 #include "colour.h"
 #include "components.h"
 
@@ -15,22 +14,46 @@ void actor_free(Actor *actor);
 
 // --- Actor Component Management ---
 
-void actor_add_health_component(Actor *actor, HealthComponent *component);
-void actor_add_combat_component(Actor *actor, CombatComponent *component);
-void actor_add_ai_component(Actor *actor, AIComponent *component);
-void actor_add_name_component(Actor *actor, NameComponent *component);
+void actor_add_health_component(Actor *actor, int max_hp);
+void actor_add_combat_component(Actor *actor, int attack_power);
+void actor_add_ai_component(Actor *actor);
+void actor_add_name_component(Actor *actor, const char *name);
 
-HealthComponent *actor_get_health_component(const Actor *actor);
-CombatComponent *actor_get_combat_component(const Actor *actor);
-AIComponent *actor_get_ai_component(const Actor *actor);
-NameComponent *actor_get_name_component(const Actor *actor);
+void actor_remove_health_component(Actor *actor);
+void actor_remove_combat_component(Actor *actor);
+void actor_remove_ai_component(Actor *actor);
+void actor_remove_name_component(Actor *actor);
+
+const HealthComponent *actor_get_health_component(const Actor *actor);
+const CombatComponent *actor_get_combat_component(const Actor *actor);
+const AIComponent *actor_get_ai_component(const Actor *actor);
+const NameComponent *actor_get_name_component(const Actor *actor);
+
+HealthComponent *actor_get_health_component_mut(Actor *actor);
+CombatComponent *actor_get_combat_component_mut(Actor *actor);
+AIComponent *actor_get_ai_component_mut(Actor *actor);
+NameComponent *actor_get_name_component_mut(Actor *actor);
 
 // --- Actor Getters/Setters ---
 
+int actor_get_x(const Actor *actor);
+int actor_get_y(const Actor *actor);
 void actor_get_position(const Actor *actor, int *x, int *y);
-void actor_set_position(Actor *actor, int x, int y);
-
 char actor_get_glyph(const Actor *actor);
 Colour actor_get_colour(const Actor *actor);
+unsigned char actor_get_r(const Actor *actor);
+unsigned char actor_get_g(const Actor *actor);
+unsigned char actor_get_b(const Actor *actor);
+unsigned char actor_get_a(const Actor *actor);
+
+void actor_set_x(Actor *actor, int x);
+void actor_set_y(Actor *actor, int y);
+void actor_set_position(Actor *actor, int x, int y);
+void actor_set_glyph(Actor *actor, char glyph);
+void actor_set_colour(Actor *actor, Colour colour);
+void actor_set_r(Actor *actor, unsigned char r);
+void actor_set_g(Actor *actor, unsigned char g);
+void actor_set_b(Actor *actor, unsigned char b);
+void actor_set_a(Actor *actor, unsigned char a);
 
 #endif // ACTOR_H
--- src/actor.c
+++ src/actor.c
@@ -1,4 +1,6 @@
 #include "actor.h"
+#include "colour.h"
+#include "components.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,14 +11,14 @@ struct actor
 {
     int x;
     int y;
-    char glyph;
     Colour colour;
+    char glyph;
 
     // Components (can be NULL)
-    HealthComponent *health;
-    CombatComponent *combat;
-    AIComponent *ai;
-    NameComponent *name;
+    HealthComponent *health_component;
+    CombatComponent *combat_component;
+    AIComponent *ai_component;
+    NameComponent *name_component;
 };
 
 // --- Actor Creation/Destruction ---
@@ -43,10 +45,10 @@ Actor *actor_create(int x, int y, char glyph, Colour colour)
 
     // Initialize all component pointers to NULL.
     // Components must be created and added separately.
-    actor->health = NULL;
-    actor->combat = NULL;
-    actor->ai = NULL;
-    actor->name = NULL;
+    actor->health_component = NULL;
+    actor->combat_component = NULL;
+    actor->ai_component = NULL;
+    actor->name_component = NULL;
 
     return actor;
 }
@@ -59,21 +61,21 @@ void actor_free(Actor *actor)
     }
 
     // Free each component if it exists.
-    if (actor->health)
+    if (actor->health_component)
     {
-        health_component_free(actor->health);
+        health_component_free(actor->health_component);
     }
-    if (actor->combat)
+    if (actor->combat_component)
     {
-        combat_component_free(actor->combat);
+        combat_component_free(actor->combat_component);
     }
-    if (actor->ai)
+    if (actor->ai_component)
     {
-        ai_component_free(actor->ai);
+        ai_component_free(actor->ai_component);
     }
-    if (actor->name)
+    if (actor->name_component)
     {
-        name_component_free(actor->name);
+        name_component_free(actor->name_component);
     }
 
     // Finally, free the actor struct itself.
@@ -82,66 +84,208 @@ void actor_free(Actor *actor)
 
 // --- Actor Component Management ---
 
-void actor_add_health_component(Actor *actor, HealthComponent *component)
+void actor_add_health_component(Actor *actor, int max_hp)
 {
-    actor->health = component;
+    if (actor->health_component)
+    {
+        fprintf(stderr, "%s: component already exists\n", __func__);
+    }
+    else
+    {
+        actor->health_component = health_component_create(max_hp);
+    }
+}
+void actor_add_combat_component(Actor *actor, int attack_power)
+{
+    if (actor->combat_component)
+    {
+        fprintf(stderr, "%s: component already exists\n", __func__);
+    }
+    else
+    {
+        actor->combat_component = combat_component_create(attack_power);
+    }
+}
+void actor_add_ai_component(Actor *actor)
+{
+    if (actor->ai_component)
+    {
+        fprintf(stderr, "%s: component already exists\n", __func__);
+    }
+    else
+    {
+        actor->ai_component = ai_component_create();
+    }
+}
+void actor_add_name_component(Actor *actor, const char *name)
+{
+    if (actor->name_component)
+    {
+        fprintf(stderr, "%s: component already exists\n", __func__);
+    }
+    else
+    {
+        actor->name_component = name_component_create(name);
+    }
 }
 
-void actor_add_combat_component(Actor *actor, CombatComponent *component)
+void actor_remove_health_component(Actor *actor)
 {
-    actor->combat = component;
+    if (!actor->health_component)
+    {
+        fprintf(stderr, "%s: component does not exists\n", __func__);
+    }
+    else
+    {
+        health_component_free(actor->health_component);
+        actor->health_component = NULL;
+    }
+}
+void actor_remove_combat_component(Actor *actor)
+{
+    if (!actor->combat_component)
+    {
+        fprintf(stderr, "%s: component does not exists\n", __func__);
+    }
+    else
+    {
+        combat_component_free(actor->combat_component);
+        actor->combat_component = NULL;
+    }
+}
+void actor_remove_ai_component(Actor *actor)
+{
+    if (!actor->ai_component)
+    {
+        fprintf(stderr, "%s: component does not exists\n", __func__);
+    }
+    else
+    {
+        ai_component_free(actor->ai_component);
+        actor->ai_component = NULL;
+    }
+}
+void actor_remove_name_component(Actor *actor)
+{
+    if (!actor->name_component)
+    {
+        fprintf(stderr, "%s: component does not exists\n", __func__);
+    }
+    else
+    {
+        name_component_free(actor->name_component);
+        actor->name_component = NULL;
+    }
 }
 
-void actor_add_ai_component(Actor *actor, AIComponent *component)
+const HealthComponent *actor_get_health_component(const Actor *actor)
 {
-    actor->ai = component;
+    return actor->health_component;
+}
+const CombatComponent *actor_get_combat_component(const Actor *actor)
+{
+    return actor->combat_component;
+}
+const AIComponent *actor_get_ai_component(const Actor *actor)
+{
+    return actor->ai_component;
+}
+const NameComponent *actor_get_name_component(const Actor *actor)
+{
+    return actor->name_component;
 }
 
-void actor_add_name_component(Actor *actor, NameComponent *component)
+HealthComponent *actor_get_health_component_mut(Actor *actor)
 {
-    actor->name = component;
+    return actor->health_component;
 }
-
-HealthComponent *actor_get_health_component(const Actor *actor)
+CombatComponent *actor_get_combat_component_mut(Actor *actor)
 {
-    return actor->health;
+    return actor->combat_component;
 }
-
-CombatComponent *actor_get_combat_component(const Actor *actor)
+AIComponent *actor_get_ai_component_mut(Actor *actor)
 {
-    return actor->combat;
+    return actor->ai_component;
 }
-
-AIComponent *actor_get_ai_component(const Actor *actor)
+NameComponent *actor_get_name_component_mut(Actor *actor)
 {
-    return actor->ai;
-}
-
-NameComponent *actor_get_name_component(const Actor *actor)
-{
-    return actor->name;
+    return actor->name_component;
 }
 
 // --- Actor Getters/Setters ---
 
+int actor_get_x(const Actor *actor)
+{
+    return actor->x;
+}
+int actor_get_y(const Actor *actor)
+{
+    return actor->y;
+}
 void actor_get_position(const Actor *actor, int *x, int *y)
 {
     *x = actor->x;
     *y = actor->y;
 }
+char actor_get_glyph(const Actor *actor)
+{
+    return actor->glyph;
+}
+Colour actor_get_colour(const Actor *actor)
+{
+    return actor->colour;
+}
+unsigned char actor_get_r(const Actor *actor)
+{
+    return actor->colour.r;
+}
+unsigned char actor_get_g(const Actor *actor)
+{
+    return actor->colour.g;
+}
+unsigned char actor_get_b(const Actor *actor)
+{
+    return actor->colour.b;
+}
+unsigned char actor_get_a(const Actor *actor)
+{
+    return actor->colour.a;
+}
 
+void actor_set_x(Actor *actor, int x)
+{
+    actor->x = x;
+}
+void actor_set_y(Actor *actor, int y)
+{
+    actor->y = y;
+}
 void actor_set_position(Actor *actor, int x, int y)
 {
     actor->x = x;
     actor->y = y;
 }
-
-char actor_get_glyph(const Actor *actor)
+void actor_set_glyph(Actor *actor, char glyph)
 {
-    return actor->glyph;
+    actor->glyph = glyph;
 }
-
-Colour actor_get_colour(const Actor *actor)
+void actor_set_colour(Actor *actor, Colour colour)
 {
-    return actor->colour;
+    actor->colour = colour;
+}
+void actor_set_r(Actor *actor, unsigned char r)
+{
+    actor->colour.r = r;
+}
+void actor_set_g(Actor *actor, unsigned char g)
+{
+    actor->colour.g = g;
+}
+void actor_set_b(Actor *actor, unsigned char b)
+{
+    actor->colour.b = b;
+}
+void actor_set_a(Actor *actor, unsigned char a)
+{
+    actor->colour.a = a;
 }
