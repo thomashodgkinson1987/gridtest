531c1ab7c92e73a2dc67c5daa7e83780996d6bae
Thomas Hodgkinson <thomashodgkinson1987@gmail.com>
2025-09-14 00:33:34 +0000

    WIP

--- dev-build.sh
+++ dev-build.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 set -e
 
-# Use the project's directory name to create a unique and predictable image tag.
-# This avoids conflicts with other projects.
 IMAGE_TAG="$(basename "$PWD")-dev:latest"
 
 echo "Building the '$IMAGE_TAG' container image..."
 
-# We no longer need --build-arg for user info.
-# The user mapping is handled dynamically at runtime.
 podman build \
     -t "$IMAGE_TAG" \
     -f Dockerfile \
